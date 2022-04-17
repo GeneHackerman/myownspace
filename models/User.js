597f048ebeede1424526688e79bfe1682b55c9bf
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-04-17 09:52:44 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: 'Please provide a username!',
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: 'Please provide an email address!',
+            unique: true,
+            match: [/.+@.+\..+/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Though'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+        },
+        {
+            toJSON: {
+                virtuals: true,
+                getters: true
+            },
+            id: false
+        }  
+);
+
+// get total count of friends comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create UserModel using UserSchema
+const User = model('User', UserSchema);
+
+module.exports = User;
